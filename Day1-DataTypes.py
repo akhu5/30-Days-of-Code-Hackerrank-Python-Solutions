439c281a20040e63bea8ca63a6fbd97d6553109c
Akhil Khandelwal <dude.akhil3@gmail.com>
2020-03-25 23:56:48 +0530

    Day1-DataTypes.py

---
+++ Day1-DataTypes.py
@@ -0,0 +1,14 @@
+# Given variables
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Variables input from the user
+user_int = int(input())
+user_float = float(input())
+user_str = str(input())
+
+# Printing after add and concatenate
+print(i + user_int)
+print(d + user_float)
+print(s + user_str)
